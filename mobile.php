171e179f59de2b0e947769ce093f452cfb967a2c
王茂林 <1290800466@qq.com>
2016-01-06 11:31:39 +0800

    轻量级宣传页

---
+++ mobile.php
@@ -0,0 +1,79 @@
+<?php
+require_once 'function.php';
+if (!isMobile()){
+    //跳转到PC端
+    header("Location: pc.php"); 
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>手机页面</title>
+	<meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0,user-scalable=no" name="viewport" id="viewport" />
+	<link rel="stylesheet" type="text/css" href="css/mobile.css" media="screen and (max-width:639px)">
+</head>
+<body>
+	<div class="banner"><img src="img/m_banner01.jpg" height="360" width="640" alt=""></div>
+	<div class="pc_txt01">
+		<h3>&lt;"轻量级"IT服务&gt;</h3>
+		<p>
+			随着信息化建设的不断深入，企业各项业务对IT的依赖不断增强，几乎所有工作都会凭借计算机网络完成，有一个高效的IT应用环境是保障工作效率的第一环，不断出现的新技术，新的工作模式甚至新行业模式也对IT服务提出了更高更细的要求。
+		</p>
+		<div class="m_bottom"></div>
+	</div>
+	<div class="banner"><img src="img/m_banner02.jpg" height="360" width="640" alt=""></div>
+	<div class="pc_txt01">
+		<h3>&lt;IT服务需求&gt;</h3>
+		<p>
+			计算机网络相对开放面对着各种风险，加上人们日常的不良操作习惯，还有爆发式增长的系统软件对硬件资源消耗，目前保持正常运行状态下的计算机也只能是常年积累垃圾，导致系统越来越慢，部分功能失效，网络迟钝甚至瘫痪等等问题。而计算机系统并没能实现真正的自我修复和免疫机制，90%以上的人会抱怨自己的计算机不好用，除了影响心情，更重要的是工作效率受到极大影响，客观上阻碍了公司的运营和良性发展。
+		</p>
+		<div class="m_bottom"></div>
+	</div>
+	<div class="banner"><img src="img/m_banner03.jpg" height="360" width="640" alt=""></div>
+	<div class="pc_txt01">
+		<h3>&lt;市场形势&gt;</h3>
+		<p>
+			虽然目前市场IT项目外包公司众多，但由于市场需求巨大，大多数服务商专业方向和行业针对性强，只承接大型公司、国有企业、行业巨头的大型服务项目，对微小型IT项目（如小型管理软件开发等），由于脱离其固有工作流程和抬高了人力成本，便不愿染指，SO能向中小企业提供小规模IT服务方案的服务商极其欠缺。能够提供类似服务的众多小公司或工作室，只侧重为客户提供如硬件维护、网站建设、程序开发、信息资讯等服务，多大以作坊式作业，存在诸多问题，公司规模小、管理不正规、人员流动性大、技术力量薄弱，不成规模，难以提供快捷、高效、持续性强并且高质量的技术支持；或者服务项目单一，不能覆盖更大服务范围。
+		</p>
+		<div class="m_bottom"></div>
+	</div>
+	<div class="banner"><img src="img/m_banner04.jpg" height="360" width="640" alt=""></div>
+	<div class="pc_txt01">
+		<h3>&lt;关于我们&gt;</h3>
+		<p>
+			我们会为您创建一个专业、稳定、安全、便捷的高品质IT工作环境！ 
+		</p>
+		<div class="m_bottom"></div>
+	</div>
+	<div class="banner"><img src="img/m_banner05.jpg" height="360" width="640" alt=""></div>
+	<div class="pc_txt01">
+		<h3>&lt;上升优势&gt;</h3>
+		<p>
+			轻量级团队有多年IT服务行业需求研究与服务经验，持续的北大硕士专业技术团队补给支持，还有国内外著名IT互联网公司高端技术人才组成的弹性人力云，可根据企业或用户实际需求快速制定IT服务方案。 
+		</p>
+		<div class="m_bottom"></div>
+	</div>
+	<div class="banner"><img src="img/m_banner06.jpg" height="360" width="640" alt=""></div>
+	<div class="pc_txt01">
+		<h3>&lt;我们能为您做&gt;</h3>
+		<p>
+			有“轻量级”的服务，以上这些都不是问题，因为您有专属的IT服务团队。一般公司做维护，而我们负责你的轻量级开发，有事您说话。我们的服务口号：写得了代码，查得出异常，杀得了木马， 翻得了围墙，轻量级的服务，海底捞的顺畅。 
+		</p>
+		<div class="m_bottom"></div>
+	</div>
+	<div class="banner"><img src="img/m_banner07.jpg" height="360" width="640" alt=""></div>
+	<div class="pc_txt01">
+		<h3>&lt;标准化服务&gt;</h3>
+		<p>
+			系统解决方案，目的是根据企业网络实际情况及时、有效提出合理的优化、升级方案书，使企业网络系统总是处于最佳状态。 
+		</p>
+		<div class="m_bottom"></div>
+	</div>
+	<div class="banner"><img src="img/m_banner08.jpg" height="360" width="640" alt=""></div>
+	<div class="pc_txt01">
+		<h3 style="font-size:12px; color:#8b8989; margin-bottom:4px;">北京轻量级科技有限公司</h3>
+	</div>
+</body>
+</body>
+</html>
